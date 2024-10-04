64e97009529eb59c9e4fe1ad3cb5144bcf0a8507
dianaUTJ <2122300420@soy.utj.edu.mx>
2024-10-04 13:02:07 -0600

    add repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, nums) {
+    if (nums < 0){
+        return "ERROR";
+    }
+    let string = "";
+    for (let i = 0; i < nums; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
