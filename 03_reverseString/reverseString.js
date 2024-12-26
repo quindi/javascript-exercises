d3e255c5d5613f505bc54a0daade20338999bbbe
dianaUTJ <diana.p.m.q@gmail.com>
2024-12-25 19:03:36 -0600

    add reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWord = "";
+    let letter = "";
 
+    for(let i = 1; i <= word.length; i++){
+        letter = word.charAt(word.length - i);
+        reverseWord += letter;
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
